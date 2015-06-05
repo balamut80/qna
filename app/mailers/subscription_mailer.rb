af155db3f6a80cec13c2b5c9a20920a06c700d39
Alexander <balamut80@hotmail.com>
2015-06-05 14:20:02 +0300

    hotfix rename SubscriptionMailer action

--- app/mailers/subscription_mailer.rb
+++ app/mailers/subscription_mailer.rb
@@ -1,7 +1,7 @@
 class SubscriptionMailer < ActionMailer::Base
 	default from: "from@example.com"
 
-	def answer(subscriber, answer)
+	def question(subscriber, answer)
 		@answer = answer
 		mail to: subscriber.email
 	end
